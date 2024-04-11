218ca23c2c109e215ef0feef42fd134cf82fef57
okuyama-code <ooyy0121@gmail.com>
2024-04-12 07:31:35 +0900

    練習をコミット

---
+++ README.md
@@ -0,0 +1,1 @@
+ts atcorder練習
---
+++ src/hello.ts
@@ -0,0 +1,10 @@
+// 次は　３
+
+const arry: (string | number)[] = ['aa',1,'cc','dd','ee','ff','gg'];
+
+// const newArry = arry.slice(3,6)
+const newArry = arry.slice(-7,-2);
+
+
+console.log(newArry)
+
